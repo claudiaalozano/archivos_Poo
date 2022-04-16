f5888d01aaf684c2b6baac332687bd5ee7d4ea12
claudiaalozano <clau.lozano.perez@gmail.com>
2022-04-16 19:22:00 +0200

    mas cambios

--- Clases/archivo.py
+++ Clases/archivo.py
@@ -1,32 +1,31 @@
 
-def leer_archivo(nombre_archivo):
-    lista_diccionarios = []
-    archivo = open(nombre_archivo, 'r')
-    for linea in archivo:
-        diccionario = {}
-        linea = linea.strip()
-        linea = linea.split(',')
-        diccionario['nombre'] = linea[0]
-        diccionario['calificacion'] = linea[1]
-        lista_diccionarios.append(diccionario)
-    archivo.close()
-    return lista_diccionarios
+#Una función que reciba el fichero de calificaciones (1).txt y devuelva una lista de diccionarios, donde cada diccionario contiene la información de los exámenes y la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.
+import csv
 
+
+def leer_fichero(nombre_fichero):
+    lista_diccionarios = []
+    with open(nombre_fichero, 'r') as fichero:
+        lector = csv.DictReader(fichero)
+        for fila in lector:
+            lista_diccionarios.append(fila)
+    return lista_diccionarios
+    
 
 def calcular_nota_final(lista_diccionarios):
-    for diccionario in lista_diccionarios:
-        calificacion_parcial = int(diccionario['calificacion'])
-        calificacion_final = (calificacion_parcial * 0.3) + (calificacion_parcial * 0.4)
-        diccionario['nota_final'] = calificacion_final
-    return lista_diccionarios
+        for diccionario in lista_diccionarios:
+            calificacion_parcial = int(diccionario['calificacion'])
+            calificacion_final = (calificacion_parcial * 0.3) + (calificacion_parcial * 0.4)
+            diccionario['nota_final'] = calificacion_final
+        return lista_diccionarios
 
 
 def aprobados_y_suspensos(lista_diccionarios):
-    lista_aprobados = []
-    lista_suspensos = []
-    for diccionario in lista_diccionarios:
-        if diccionario['nota_final'] >= 5:
-            lista_aprobados.append(diccionario)
-        else:
-            lista_suspensos.append(diccionario)
-    return lista_aprobados, lista_suspensos
+        lista_aprobados = []
+        lista_suspensos = []
+        for diccionario in lista_diccionarios:
+            if diccionario['nota_final'] >= 5:
+                lista_aprobados.append(diccionario)
+            else:
+                lista_suspensos.append(diccionario)
+        return lista_aprobados, lista_suspensos
